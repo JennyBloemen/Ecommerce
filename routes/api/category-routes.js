8aa15e44ccdc756060dd993f511c7380a228dbea
Jenny Bloemen <jennyjbloemen@gmail.com>
2022-11-19 17:26:08 -0700

    Create Category route test and working

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -31,16 +31,27 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", (req, res) => {
-  // create a new category
+// create a new category
+router.post("/", async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: "No category found!" });
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put("/:id", (req, res) => {
-  // update a category by its `id` value
-});
+// update a category by its `id` value
+router.put("/:id", (req, res) => {});
 
-router.delete("/:id", (req, res) => {
-  // delete a category by its `id` value
-});
+// delete a category by its `id` value
+router.delete("/:id", (req, res) => {});
 
 module.exports = router;
