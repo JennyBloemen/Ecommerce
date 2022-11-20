d479558c5bc501755ac81d5c131f6d18f10cf403
Jenny Bloemen <jennyjbloemen@gmail.com>
2022-11-20 16:20:08 -0700

    removed unnessary code

--- models/index.js
+++ models/index.js
@@ -7,23 +7,23 @@ const ProductTag = require("./ProductTag");
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: "category_id",
+  onDelete: "SET NULL",
 });
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: "category_id",
-  onDelete: "CASCADE",
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: ProductTag,
-  foreignKey: "product_id",
-  otherKey: "tag_id",
+  through: {
+    model: ProductTag,
+  },
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: ProductTag,
-  foreignKey: "tag_id",
-  otherKey: "product_id",
+  through: {
+    model: ProductTag,
+  },
 });
 
 module.exports = {
