bc2adf579feb9a642263540482e45b51daee631c
Jenny Bloemen <jennyjbloemen@gmail.com>
2022-11-20 14:32:33 -0700

    updated put route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -34,14 +34,7 @@ router.get("/:id", async (req, res) => {
 // create a new category
 router.post("/", async (req, res) => {
   try {
-    const categoryData = await Category.create(req.body, {
-      where: {
-        id: req.params.id,
-      },
-    });
-    if (!categoryData) {
-      res.status(404).json({ message: "No category found!" });
-    }
+    const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
